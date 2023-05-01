fd0f3a1e25688cd5eb68746aa34d730b51e205c1
Bibek Bhujel <biekbhujel077@gmail.com>
2023-05-01 07:47:03 +0545

    Baki workout card completed

--- app/src/main/java/com/example/workoutwithbaki/MainActivity.kt
+++ app/src/main/java/com/example/workoutwithbaki/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.workoutwithbaki
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.workoutwithbaki.ui.theme.WorkoutWithBakiTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    BakiWorkoutCard()
                 }
             }
         }
@@ -40,23 +48,50 @@ fun BakiWorkoutList() {
 }
 
 @Composable
-fun BakiWorkoutCard() {
-
+fun BakiWorkoutCard(modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(15.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.day),
+                style = MaterialTheme.typography.h2
+            )
+            BakiWorkoutTitle()
+            BakiWorkoutImage()
+            BakiWorkoutDescription()
+        }
+    }
 }
+
 
 @Composable
 fun BakiWorkoutTitle() {
-
+    Text(
+        text = stringResource(id = R.string.title1),
+        style = MaterialTheme.typography.h3
+    )
 }
 
 @Composable
-fun BakiWorkoutImage() {
-
+fun BakiWorkoutImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.baki1),
+        contentDescription = null,
+        contentScale = ContentScale.Fit,
+        modifier = modifier.fillMaxWidth()
+    )
 }
 
 @Composable
 fun BakiWorkoutDescription() {
-
+    Text(
+        text = stringResource(id = R.string.description1),
+        textAlign = TextAlign.Justify
+    )
 }
 
 @Composable
@@ -68,5 +103,6 @@ fun BakiWorkoutAppBar() {
 @Composable
 fun DefaultPreview() {
     WorkoutWithBakiTheme {
+        BakiWorkoutCard()
     }
 }
